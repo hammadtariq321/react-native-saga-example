d6927ccc35ffdf2e18a8f10f2b830c6c20153c28
msaqibdev <msaqibdev@gmail.com>
2023-02-27 16:31:26 +0500

    update#2

--- components/Counter.js
+++ components/Counter.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Button, Text, View } from "react-native";
+import { ActivityIndicator, Button, Text, View } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import { increment, decrement } from "../redux/slices/counterSlice";
 import { fetchUserData } from "../redux/slices/usersSlice";
@@ -8,6 +8,7 @@ export default function Counter() {
   const dispatch = useDispatch();
   const counterValue = useSelector((state) => state.counter.data);
   const userData = useSelector((state) => state.users.users);
+  const loading = useSelector((state) => state.users.loading);
   console.log("counterValue", counterValue);
 
   useEffect(() => {
@@ -19,7 +20,9 @@ export default function Counter() {
       <Button title="Increment" onPress={() => dispatch(increment(2))} />
       <Text>Counter: {counterValue}</Text>
       <Button title="Decrement" onPress={() => dispatch(decrement(2))} />
-
+      {
+        loading && <ActivityIndicator size={'large'} color={'#000'}/>
+      }
       <View>
         {userData.map((user, index) => {
           return <Text key={index}>{user.name}</Text>;
