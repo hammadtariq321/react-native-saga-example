4d9bc1aee9e6b3047f5f3bed20ed2c535db29d0a
msaqibdev <msaqibdev@gmail.com>
2023-02-27 12:09:00 +0500

    compelete changes in saga

--- redux/slices/counterSlice.js
+++ redux/slices/counterSlice.js
@@ -3,15 +3,18 @@ import { createSlice } from "@reduxjs/toolkit";
 export const counterSlice = createSlice({
   name: "counter",
   initialState: {
-    value: 4,
+    data: 4,
   },
   reducers: {
-    increment: (state) => {
-      state.data += 1
+    increment: (state, action) => {
+      state.data += action.payload;
+    },
+    decrement: (state, action) => {
+      state.data -= action.payload;
     },
   },
 });
 
-export const { increment } = counterSlice.actions;
+export const { increment, decrement } = counterSlice.actions;
 
 export default counterSlice.reducer;
